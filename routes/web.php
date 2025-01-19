1946a85406a1e42b1e53361c1fd4a088d66c2bff
locwa <94048557+locwa@users.noreply.github.com>
2025-01-19 18:29:01 +0800

    created the other pages and routed them accordingly

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('/fleet', function () {
+    return view('fleet');
+});
+
+Route::get('services', function () {
+    return view('services');
+});
