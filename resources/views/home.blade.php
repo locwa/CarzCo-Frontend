d5f958f07bf883b809dcbca0011b058c9476a63d
locwa <94048557+locwa@users.noreply.github.com>
2025-01-20 07:54:40 +0800

    added main body

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,15 @@
 <x-layout>
-    <main>
-        <div class="hero">
-            <div class="heading">
-                <h1 class="f-color-white f-size-xl">Rent Exotic and Luxury Cars</h1>
-                <p class="f-color-white f-size-md f-weight-light margin-small">Discover our fleet</p>
-            </div>
-            <div class="hero-spacer"></div>
-            <input type="text" name="vehicle-search" id="vehicleSearch" class="vehicle-search f-size-sm f-color-white" placeholder="Search by Make or Model" >
+    <header class="hero">
+        <div class="heading">
+            <h1 class="f-color-white f-size-xl">Rent Exotic and Luxury Cars</h1>
+            <p class="f-color-white f-size-md f-weight-light mt-sm">Discover our fleet</p>
         </div>
+        <div class="hero-spacer"></div>
+        <input type="text" name="vehicle-search" id="vehicleSearch" class="vehicle-search f-size-sm f-color-white" placeholder="Search by Make or Model" >
+    </header>
+    <main>
+        <h1 class="f-size-lg mt-lg">At CarzCo, We Provide Top Quality Exotic and Luxury Cars for Rent</h1>
+        <p class="f-size-md f-weight-light mt-sm">We always provide a variety of Exotic and Luxury cars that are in Top Shape and a Reasonable Price</p>
+        <hr class="mt-lg mb-lg">
     </main>
 </x-layout>
