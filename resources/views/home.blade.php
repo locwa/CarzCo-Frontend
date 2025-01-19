0dc5cc0f289e64d28809c706d9c932159158ff23
locwa <94048557+locwa@users.noreply.github.com>
2025-01-19 18:12:11 +0800

    added search bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,10 @@
         <div class="hero">
             <div class="heading">
                 <h1 class="f-color-white f-size-xl">Rent Exotic and Luxury Cars</h1>
-                <br>
-                <p class="f-color-white f-size-md f-weight-light">Discover our fleet</p>
+                <p class="f-color-white f-size-md f-weight-light margin-small">Discover our fleet</p>
             </div>
+            <div class="hero-spacer"></div>
+            <input type="text" name="" id="">
         </div>
     </main>
 
