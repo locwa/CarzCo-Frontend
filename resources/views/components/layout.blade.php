362d318a08a1c25135b97aa98697b7fa1fa11fec
locwa <94048557+locwa@users.noreply.github.com>
2025-01-23 10:11:26 +0800

    added footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,5 +19,36 @@
         </div>
     </nav>
     {{ $slot }}
+    <footer>
+        <div class="footer-main mb-lg">
+            <img src="./images/logo_main.svg" class="logo-xl">
+            <div class="footer-nav-links">
+                <div class="footer-columns">
+                    <h6 class="f-size-md f-color-white mb-sm">CarzCo</h6>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">Our Fleet</a>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">Services</a>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">About Us</a>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">Contact Us</a>
+                </div>
+                <div class="footer-columns">
+                    <h6 class="f-size-md f-color-white mb-sm">Popular Brands</h6>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">Lamborghini</a>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">Rolls Royce</a>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">Ferrari</a>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">Porsche</a>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">McLaren</a>
+                </div>
+                <div class="footer-columns">
+                    <h6 class="f-size-md f-color-white mb-sm">Services</h6>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">Chauffeur Service</a>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">Reservations</a>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">Weddings</a>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">Events</a>
+                    <a href="./" class="f-size-md f-weight-light f-color-white">CarzCo Memberships</a>
+                </div>
+            </div>
+        </div>
+        <p class="f-size-xs f-color-white">Copyright 2025, CarzCo LLC.</p>
+    </footer>
 </body>
 </html>
