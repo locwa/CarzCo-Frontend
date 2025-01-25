8d68a640b3974b64a25a3908dfa91e7ec8fd41b5
locwa <94048557+locwa@users.noreply.github.com>
2025-01-25 19:00:56 +0800

    refined containers

--- resources/views/components/containers.blade.php
+++ resources/views/components/containers.blade.php
@@ -1,11 +1,18 @@
 @props([
-    "type" => 'solid',
     "height" => "md",
     "width" => "md",
+    "hasBorder" => false,
+    "isBorderRounded" => false,
 ])
 
 @php
     $defaultClasses = " container-height-". $height . " container-width-". $width;
+    if($hasBorder){
+        $defaultClasses .= " has-border";
+    }
+    if($isBorderRounded){
+        $defaultClasses .= " border-rounded";
+    }
 @endphp
 
 <div {{ $attributes->merge(['class' => $defaultClasses]) }}>
