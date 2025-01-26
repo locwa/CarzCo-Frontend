e476c676b8354cac1aab265db13fc18300e87e5c
locwa <94048557+locwa@users.noreply.github.com>
2025-01-26 08:45:12 +0800

    added contact form

--- resources/views/components/contact-window.blade.php
+++ resources/views/components/contact-window.blade.php
@@ -14,4 +14,16 @@
 @endphp
 
 <x-containers {{ $attributes->merge(['class' => $defaultClass]) }} width="{{ $width }}" height="{{ $height }}" :hasBorder="true" :isBorderRounded="true">
+    <form action="POST" action="./contact" class="p-lg">
+        <h1>{{ $header }}</h1>
+        <label for="name">Name</label>
+        <input type="text" name="name" id="">
+        <label for="name">Email</label>
+        <input type="email" name="email" id="">
+        <label for="phone">Phone</label>
+        <input type="number" name="phone" id="">
+        <label for="message">Message</label>
+        <input type="text" name="message" id="">
+        <x-buttons type="submit" width="lg">Send</x-buttons>
+    </form>
 </x-containers>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,10 @@
 <x-layout>
     <main class="contact-page">
-        <h1 class="f-size-xl mt-lg mb-sm">Contact Us</h1>
-        <p class="f-size-md f-weight-light">Get in touch with us</p>
-        <x-contact-window width="xxl" height="lg"></x-contact-window>
+        <h1 class="f-size-xl mt-lg mb-sm">Get in Touch With Us</h1>
+        <p class="f-size-md f-weight-light">Feel free to get in touch with us using the form below. Once received, we will try to assist you and arrange for your exotic car or service</p>
+        <x-contact-window width="xxl" height="lg">
+            <x-slot:header>Contact Us</x-slot:header>
+        </x-contact-window>
     </main>
 </x-layout>
 
