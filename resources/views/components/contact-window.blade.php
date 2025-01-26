a33199b6a82097c9e4800ba75f1a4d547ec6868b
locwa <94048557+locwa@users.noreply.github.com>
2025-01-26 09:17:43 +0800

    refined contact component

--- resources/views/components/contact-window.blade.php
+++ resources/views/components/contact-window.blade.php
@@ -5,7 +5,7 @@
 ])
 
 @php
-    $defaultClass = "m-lg";
+    $defaultClass = "m-lg b-width-sm";
 
     // checks if component is used as normal element or popup window
     if($type === "popup"){
@@ -14,16 +14,24 @@
 @endphp
 
 <x-containers {{ $attributes->merge(['class' => $defaultClass]) }} width="{{ $width }}" height="{{ $height }}" :hasBorder="true" :isBorderRounded="true">
-    <form action="POST" action="./contact" class="p-lg">
-        <h1>{{ $header }}</h1>
-        <label for="name">Name</label>
-        <input type="text" name="name" id="">
-        <label for="name">Email</label>
-        <input type="email" name="email" id="">
-        <label for="phone">Phone</label>
-        <input type="number" name="phone" id="">
-        <label for="message">Message</label>
-        <input type="text" name="message" id="">
-        <x-buttons type="submit" width="lg">Send</x-buttons>
+    <form action="POST" action="./contact" class="m-md contact-form">
+        <h1 class="mb-md">{{ $header }}</h1>
+        <div class="info-field-container mb-md mt-sm">
+            <div class="info-field">
+                <label for="name" class="mb-sf-weight-lightm f-weight-light">Name</label>
+                <input type="text" name="name" id="" class="input-h-md input-w-md has-border b-width-sm border-rounded">
+            </div>
+            <div class="info-field">
+                <label for="name" class="mb-sm f-weight-light">Email</label>
+                <input type="email" name="email" id="" class="input-h-md input-w-md has-border b-width-sm border-rounded">
+            </div>
+            <div class="info-field">
+                <label for="phone" class="mb-sm f-weight-light">Phone</label>
+                <input type="number" name="phone" id="" class="input-h-md input-w-md has-border b-width-sm border-rounded">
+            </div>
+        </div>
+        <label for="message" class="mb-sm f-weight-light">Message</label>
+        <textarea name="message" class="input-h-xl input-w-xl p-sm has-border b-width-sm border-rounded"></textarea>
+        <x-buttons type="submit" width="lg" class="mt-md mb-sm has-border b-width-sm">Send</x-buttons>
     </form>
 </x-containers>
