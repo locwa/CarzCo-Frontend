e9dc4db03d3a254bed449be75eb1361a3dfe7fc4
locwa <94048557+locwa@users.noreply.github.com>
2025-02-07 20:08:36 +0800

    restructured most of frontend files

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,30 @@
+<footer>
+    <div class="footer-main mb-lg">
+        <img src="./images/logo_main.svg" class="logo-xl">
+        <div class="footer-nav-links">
+            <div class="footer-columns">
+                <h6 class="f-size-md f-color-white mb-sm">CarzCo</h6>
+                <a href="../components" class="f-size-md f-weight-light f-color-white">Our Fleet</a>
+                <a href="./services" class="f-size-md f-weight-light f-color-white">Services</a>
+                <a href="./about" class="f-size-md f-weight-light f-color-white">About Us</a>
+                <a href="./contact" class="f-size-md f-weight-light f-color-white">Contact Us</a>
+            </div>
+            <div class="footer-columns">
+                <h6 class="f-size-md f-color-white mb-sm">Popular Brands</h6>
+                <a href="../components" class="f-size-md f-weight-light f-color-white">Lamborghini</a>
+                <a href="../components" class="f-size-md f-weight-light f-color-white">Rolls Royce</a>
+                <a href="../components" class="f-size-md f-weight-light f-color-white">Ferrari</a>
+                <a href="../components" class="f-size-md f-weight-light f-color-white">Porsche</a>
+                <a href="../components" class="f-size-md f-weight-light f-color-white">McLaren</a>
+            </div>
+            <div class="footer-columns">
+                <h6 class="f-size-md f-color-white mb-sm">Socials</h6>
+                <a href="../components" class="f-size-md f-weight-light f-color-white">Facebook</a>
+                <a href="../components" class="f-size-md f-weight-light f-color-white">Instagram</a>
+                <a href="../components" class="f-size-md f-weight-light f-color-white">Youtube</a>
+                <a href="../components" class="f-size-md f-weight-light f-color-white">Linkedin</a>
+            </div>
+        </div>
+    </div>
+    <p class="f-size-xs f-color-white">Copyright statement here</p>
+</footer>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <a href="../components"><img src="./images/logo_main.svg" class="logo"></a>
+    <div class="nav-links">
+        <a href="./fleet" class="f-color-white f-size-md">Our Fleet</a>
+        <a href="./services" class="f-color-white f-size-md">Services</a>
+        <a href="./about" class="f-color-white f-size-md">About</a>
+    </div>
+</nav>
