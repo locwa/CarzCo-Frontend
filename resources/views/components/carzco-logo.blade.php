d8472ce7f4b0ad11fc962bc750b55e23fd0af115
locwa <94048557+locwa@users.noreply.github.com>
2025-02-08 13:21:00 +0800

    made some letter change color based on theme

--- resources/views/components/carzco-logo.blade.php
+++ resources/views/components/carzco-logo.blade.php
@@ -5,7 +5,7 @@
     xml:space="preserve"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:svg="http://www.w3.org/2000/svg"
-    class="h-18 w-52">
+    {{ $attributes->merge(['class' => 'h-18 w-52']) }}>
     <defs
         id="defs1" /><g
         id="layer1"><g
