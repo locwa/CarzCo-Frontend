514bef503391750ed8aa9a97dc8a162bcbcfd297
locwa <94048557+locwa@users.noreply.github.com>
2025-02-08 14:01:22 +0800

    restyled containers

--- resources/views/components/containers.blade.php
+++ resources/views/components/containers.blade.php
@@ -1,20 +1,4 @@
-@props([
-    "height" => "md",
-    "width" => "md",
-    "hasBorder" => false,
-    "isBorderRounded" => false,
-])
-
-@php
-    $defaultClasses = " container-height-". $height . " container-width-". $width;
-    if($hasBorder){
-        $defaultClasses .= " has-border";
-    }
-    if($isBorderRounded){
-        $defaultClasses .= " border-rounded";
-    }
-@endphp
-
-<div {{ $attributes->merge(['class' => $defaultClasses]) }}>
+<div class="h-80 w-80 bg-[#0f0f0f] dark:bg-gray-100 text-gray-100 dark:text-[#0f0f0f] rounded flex flex-col items-center justify-center p-5">
+    <h4 class="text-2xl text-center font-extrabold p-2">{{ $title }}</h4>
     {{ $slot }}
 </div>
