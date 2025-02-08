43535c83acad10ae2bda15040acd5b5e109a48b9
locwa <94048557+locwa@users.noreply.github.com>
2025-02-08 23:16:59 +0800

    added size option for primary button

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,5 +1,6 @@
 @props([
-   'inverted' => false
+   'inverted' => false,
+   'size' => 'md'
 ])
 
 @php
@@ -9,8 +10,22 @@
     } else {
         $colors = 'bg-[#0f0f0f] dark:bg-gray-200 text-white dark:text-[#0f0f0f] hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:ring-offset-2 dark:focus:ring-offset-gray-800';
     }
+
+    $padding = "";
+    switch($size){
+        case 'lg':
+            $padding = "px-8 py-4";
+            break;
+        case 'md':
+            $padding = "px-4 py-2";
+            break;
+        case 'sm':
+            $padding = "px-2 py-1";
+            break;
+    }
 @endphp
 
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-8 py-4 '. $colors .' border border-transparent rounded-md font-semibold text-md focus:outline-none focus:ring-2 focus:ring-indigo-500 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center '. $padding .' '. $colors .' border border-transparent rounded-md font-semibold text-md focus:outline-none focus:ring-2 focus:ring-indigo-500 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
+
