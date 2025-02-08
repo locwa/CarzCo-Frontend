ab4f3b5a916d5a9d3cee11c6be28b32385aeda65
locwa <94048557+locwa@users.noreply.github.com>
2025-02-08 10:41:33 +0800

    made page title dynamic and added footer component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ $title }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -18,13 +18,15 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
-<div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+<div class="min-h-screen bg-gray-100 dark:bg-[#0f0f0f]">
     @include('layouts.navigation')
 
     <!-- Page Content -->
     <main>
         {{ $slot }}
     </main>
+
+    <!-- @include('layouts.footer') -->
 </div>
 </body>
 </html>
