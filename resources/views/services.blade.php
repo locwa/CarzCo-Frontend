1db8c12092a37856f6c7a7dc60076b18293951b9
locwa <94048557+locwa@users.noreply.github.com>
2025-02-08 18:31:14 +0800

    remade services page

---
+++ resources/views/services.blade.php
@@ -0,0 +1,43 @@
+<x-app-layout>
+    <x-slot:title>Services | CarzCo</x-slot:title>
+    <div class="flex flex-col items-center">
+        <h1 class="text-5xl font-extrabold pt-10">Chaffeur Services</h1>
+        <h3 class="pb-10 text-center">Experience the pinnacle of luxury travel with CarzCo's professional chauffeur service.</h3>
+        <p class="text-xl text-center pb-10">Our experienced and discreet chauffeurs will ensure your safe and comfortable transportation in a selection of luxurious vehicles, from executive sedans to spacious SUVs. Whether you require airport transfers, corporate travel, or simply a relaxing evening out, our chauffeur service provides a touch of elegance and convenience to any occasion.</p>
+        <a href="./contact">
+            <x-primary-button class="mb-10">Contact Us</x-primary-button>
+        </a>
+    </div>
+    <hr class="mx-32 my-10">
+    <div class="flex flex-col items-center px-10">
+        <h1 class="text-5xl font-extrabold text-center pt-10">Reservations</h1>
+        <h3 class="pb-10">Effortlessly book your dream driving experience with CarzCo's convenient reservation system.</h3>
+        <p class="text-xl text-center pb-10">Our user-friendly online platform and dedicated reservation team make it easy to browse our fleet, compare prices, and secure your preferred vehicle and dates. We offer flexible booking options and personalized assistance to ensure your rental experience is seamless from start to finish.</p>
+        <a href="./contact">
+            <x-primary-button class="mb-10">Contact Us</x-primary-button>
+        </a>
+    </div>
+    <hr class="mx-32 my-10">
+    <div class="flex flex-col items-center px-10 text-lg">
+        <h1 class="text-5xl font-extrabold text-center px-10">Events</h1>
+        <h3 class="pb-10 text-md">Elevate your special event with a touch of automotive flair</h3>
+        <p class="mb-2 text-2xl font-bold">CarzCo provides a range of event services, including:</p>
+        <ul class="mb-10">
+            <li class="list-disc"><strong>Showstopping vehicle displays:</strong> Impress your guests with a curated selection of exotic and luxury vehicles at your next corporate event, wedding, or private party.</li>
+            <li class="list-disc"><strong>VIP transportation:</strong> Provide your guests with exclusive transportation in style with our luxurious chauffeur service.</li>
+            <li class="list-disc"><strong>Themed driving experiences:</strong> Offer unique and thrilling driving experiences as part of your event, such as exhilarating track days or scenic tours.</li>
+        </ul>
+        <a href="./contact">
+            <x-primary-button class="mb-10">Contact Us</x-primary-button>
+        </a>
+    </div>
+    <hr class="mx-32 my-10">
+    <div class="flex flex-col items-center px-10 text-lg">
+        <h1 class="text-5xl font-extrabold text-center pt-10">CarzCo Memberships</h1>
+        <h3 class="pb-10">Unlock exclusive benefits and rewards with a CarzCo Membership.</h3>
+        <p class="text-center mb-10">Enjoy exclusive discounts, priority booking, and personalized service with our exclusive membership program. Members also gain access to special offers, invitations to exclusive events, and a dedicated member concierge to assist with all your transportation needs.</p>
+        <a href="./contact">
+            <x-primary-button class="mb-10">Contact Us</x-primary-button>
+        </a>
+    </div>
+</x-app-layout>
