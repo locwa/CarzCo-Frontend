7c96132dba095a5fd03f123c78a5930c14fc9b78
locwa <94048557+locwa@users.noreply.github.com>
2025-02-08 19:58:49 +0800

    added size options to containers

--- resources/views/components/containers.blade.php
+++ resources/views/components/containers.blade.php
@@ -1,4 +1,26 @@
-<div class="h-80 w-80 bg-[#0f0f0f] dark:bg-gray-100 text-gray-100 dark:text-[#0f0f0f] rounded flex flex-col items-center justify-center p-5">
-    <h4 class="text-2xl text-center font-extrabold p-2">{{ $title }}</h4>
+@props([
+    'size' => 'md',
+])
+
+@php
+    $heightValue = '';
+    $widthValue = '';
+
+    switch($size){
+        case 'md':
+            $heightValue = "h-80";
+            $widthValue = "w-80";
+            break;
+        case 'xl':
+            $heightValue = "h-[80vh]";
+            $widthValue = "w-2/3";
+            break;
+    }
+@endphp
+
+<div {{ $attributes->merge(['class' => "bg-[#0f0f0f] dark:bg-gray-100 text-gray-100 dark:text-[#0f0f0f] rounded flex flex-col items-center justify-center p-5 " . $heightValue . " " . $widthValue]) }}>
+    @isset($title)
+        <h4 class="text-2xl text-center font-extrabold p-2">{{ $title }}</h4>
+    @endisset
     {{ $slot }}
 </div>
