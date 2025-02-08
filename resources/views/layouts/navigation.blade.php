ad53507d597b9a5011f77e812a7d6def21a9dddf
locwa <94048557+locwa@users.noreply.github.com>
2025-02-08 13:19:48 +0800

    refined styling

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,8 +1,8 @@
 <nav class="flex justify-between p-4 items-center">
     <a href="./"><x-carzco-logo></x-carzco-logo></a>
     <div class="flex gap-10 mr-6">
-        <a href="./fleet" class="dark:text-gray-100 text-lg ">Our Fleet</a>
-        <a href="./services" class="dark:text-gray-100 text-lg ">Services</a>
-        <a href="./about" class="dark:text-gray-100 text-lg ">About</a>
+        <a href="./fleet" class="text-[#0f0f0f] dark:text-gray-100 text-lg ">Our Fleet</a>
+        <a href="./services" class="text-[#0f0f0f] dark:text-gray-100 text-lg ">Services</a>
+        <a href="./about" class="text-[#0f0f0f] dark:text-gray-100 text-lg ">About</a>
     </div>
 </nav>
