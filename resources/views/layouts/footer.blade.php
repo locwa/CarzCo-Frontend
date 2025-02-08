e78283168c1a5d842d705735660843db50d10dcc
locwa <94048557+locwa@users.noreply.github.com>
2025-02-08 13:20:29 +0800

    restyled footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,30 +1,30 @@
-<footer>
-    <div class="footer-main mb-lg">
-        <img src="./images/logo_main.svg" class="logo-xl">
-        <div class="footer-nav-links">
-            <div class="footer-columns">
-                <h6 class="f-size-md f-color-white mb-sm">CarzCo</h6>
-                <a href="../components" class="f-size-md f-weight-light f-color-white">Our Fleet</a>
-                <a href="./services" class="f-size-md f-weight-light f-color-white">Services</a>
-                <a href="./about" class="f-size-md f-weight-light f-color-white">About Us</a>
-                <a href="./contact" class="f-size-md f-weight-light f-color-white">Contact Us</a>
+<footer class="px-20 pt-10 pb-5">
+    <div class="flex justify-between">
+        <x-carzco-logo></x-carzco-logo>
+        <div class="flex gap-20">
+            <div class="flex flex-col text-[#0f0f0f] dark:text-gray-100">
+                <h6 class="text-3xl font-extrabold pb-2">CarzCo</h6>
+                <a href="../components" class="text-xl py-1">Our Fleet</a>
+                <a href="./services" class="text-xl py-1">Services</a>
+                <a href="./about" class="text-xl py-1">About Us</a>
+                <a href="./contact" class="text-xl py-1">Contact Us</a>
             </div>
-            <div class="footer-columns">
-                <h6 class="f-size-md f-color-white mb-sm">Popular Brands</h6>
-                <a href="../components" class="f-size-md f-weight-light f-color-white">Lamborghini</a>
-                <a href="../components" class="f-size-md f-weight-light f-color-white">Rolls Royce</a>
-                <a href="../components" class="f-size-md f-weight-light f-color-white">Ferrari</a>
-                <a href="../components" class="f-size-md f-weight-light f-color-white">Porsche</a>
-                <a href="../components" class="f-size-md f-weight-light f-color-white">McLaren</a>
+            <div class="flex flex-col text-[#0f0f0f] dark:text-gray-100">
+                <h6 class="text-3xl font-extrabold pb-2">Socials</h6>
+                <a href="../components" class="text-xl py-1">Facebook</a>
+                <a href="../components" class="text-xl py-1">Instagram</a>
+                <a href="../components" class="text-xl py-1">Youtube</a>
+                <a href="../components" class="text-xl py-1">Linkedin</a>
             </div>
-            <div class="footer-columns">
-                <h6 class="f-size-md f-color-white mb-sm">Socials</h6>
-                <a href="../components" class="f-size-md f-weight-light f-color-white">Facebook</a>
-                <a href="../components" class="f-size-md f-weight-light f-color-white">Instagram</a>
-                <a href="../components" class="f-size-md f-weight-light f-color-white">Youtube</a>
-                <a href="../components" class="f-size-md f-weight-light f-color-white">Linkedin</a>
+            <div class="flex flex-col text-[#0f0f0f] dark:text-gray-100">
+                <h6 class="text-3xl font-extrabold pb-2">Popular Brands</h6>
+                <a href="../components" class="text-xl py-1 hover:text-gray-400">Lamborghini</a>
+                <a href="../components" class="text-xl py-1">Rolls Royce</a>
+                <a href="../components" class="text-xl py-1">Ferrari</a>
+                <a href="../components" class="text-xl py-1">Porsche</a>
+                <a href="../components" class="text-xl py-1">McLaren</a>
             </div>
         </div>
     </div>
-    <p class="f-size-xs f-color-white">Copyright statement here</p>
+    <p class="text-xs text-[#0f0f0f] dark:text-gray-100 text-center pt-8">Copyright statement here</p>
 </footer>
