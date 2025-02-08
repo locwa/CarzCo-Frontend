99ae0b53c53a7ae8aeefec5afe21402085029fee
locwa <94048557+locwa@users.noreply.github.com>
2025-02-09 07:21:15 +0800

    added image carousel

---
+++ resources/views/components/image-carousel.blade.php
@@ -0,0 +1,36 @@
+@props([
+    'photoFileHeader',
+    'photoCount',
+    'carInfo'
+])
+
+<div class="mb-10 flex justify-center" id="imagesContainer">
+    <x-img-nav-button imgCount="{{ $photoCount }}" position="left" imgHeader="{{ $photoFileHeader }}"></x-img-nav-button>
+        <img id="carPhoto" class="h-1/3 w-2/4" src="../storage/cars/{{ $photoFileHeader . '0' . '.jpg'}}" alt="{{ $carInfo }}">
+    <x-img-nav-button imgCount="{{ $photoCount }}" position="right" imgHeader="{{ $photoFileHeader }}"></x-img-nav-button>
+</div>
+
+<script>
+    let index = 0;
+
+    function previousButton (photoHeader, imgCount){
+        let image = document.getElementById('carPhoto');
+        if (index > 0){
+            image.src = `../storage/cars/${photoHeader}${index - 1}.jpg`;
+            index--;
+        } else if (index === 0){
+            image.src = `../storage/cars/${photoHeader}${imgCount - 1}.jpg`
+            index = imgCount - 1;
+        }
+    }
+    function nextButton (photoHeader, imgCount){
+        let image = document.getElementById('carPhoto');
+        if (index !== (imgCount - 1)){
+            image.src = `../storage/cars/${photoHeader}${index + 1}.jpg`;
+            index++;
+        } else if (index === (imgCount - 1)){
+            image.src = `../storage/cars/${photoHeader}0.jpg`;
+            index = 0;
+        }
+    }
+</script>
