e225c20f9f7c8a52793a98cd1295d6bfdddf3e7a
locwa <94048557+locwa@users.noreply.github.com>
2025-02-08 23:31:10 +0800

    made photos a fixed direction

--- resources/views/fleet.blade.php
+++ resources/views/fleet.blade.php
@@ -3,23 +3,24 @@
     <div class="h-[100vh] flex gap-10 p-10">
         <div class="dark:bg-[#1f1f1f] w-1/4 rounded-[20px]"></div>
         <div class="flex gap-10">
+
             @foreach($fleet_list as $list)
                 <div class="w-80 h-3/5 p-4 dark:bg-[#1f1f1f] rounded-[20px]">
-                    <img src="./storage/cars/{{ $list->value('photo_file_header') }}0.jpg" alt="car picture" class="rounded-[15px] mb-4">
-                    <p class="text-md">{{ $list->value('year') }}</p>
-                    <h4 class="text-3xl font-extrabold">{{ $list->value('make') }}</h4>
-                    <p class="text-xl">{{ $list->value('model') }}</p>
+                    <img src="./storage/cars/{{ $list->photo_file_header }}0.jpg" alt="car picture" class="object-cover rounded-[15px] mb-4 h-48 w-auto">
+                    <p class="text-md">{{ $list->year }}</p>
+                    <h4 class="text-3xl font-extrabold">{{ $list->make }}</h4>
+                    <p class="text-xl">{{ $list->model }}</p>
 
                     <hr class="my-3">
 
                     <div class="flex justify-between">
                         <div>
                             <p class="text-md">Rent Price:</p>
-                            <h5 class="text-2xl font-bold">{{ numfmt_format_currency(numfmt_create('en_US', NumberFormatter::CURRENCY), $list->value('rent_price'),"USD") }}</h5>
+                            <h5 class="text-2xl font-bold">{{ numfmt_format_currency(numfmt_create('en_US', NumberFormatter::CURRENCY), $list->rent_price,"USD") }}</h5>
                         </div>
                     </div>
 
-                    <a href="./view-car/{{ $list->value('id') }}">
+                    <a href="./view-car/{{ $list->id }}">
                         <x-primary-button size="md" class="my-4">View Car</x-primary-button>
                     </a>
                 </div>
