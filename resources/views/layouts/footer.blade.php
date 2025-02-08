d9ee776548ddff22d45372189c1cfaea47b381f4
locwa <94048557+locwa@users.noreply.github.com>
2025-02-08 13:34:25 +0800

    restyled and fixed hrefs in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,25 +4,25 @@
         <div class="flex gap-20">
             <div class="flex flex-col text-[#0f0f0f] dark:text-gray-100">
                 <h6 class="text-3xl font-extrabold pb-2">CarzCo</h6>
-                <a href="../components" class="text-xl py-1">Our Fleet</a>
+                <a href="./fleet" class="text-xl py-1">Our Fleet</a>
                 <a href="./services" class="text-xl py-1">Services</a>
                 <a href="./about" class="text-xl py-1">About Us</a>
                 <a href="./contact" class="text-xl py-1">Contact Us</a>
             </div>
             <div class="flex flex-col text-[#0f0f0f] dark:text-gray-100">
                 <h6 class="text-3xl font-extrabold pb-2">Socials</h6>
-                <a href="../components" class="text-xl py-1">Facebook</a>
-                <a href="../components" class="text-xl py-1">Instagram</a>
-                <a href="../components" class="text-xl py-1">Youtube</a>
-                <a href="../components" class="text-xl py-1">Linkedin</a>
+                <a href="./" class="text-xl py-1">Facebook</a>
+                <a href="./" class="text-xl py-1">Instagram</a>
+                <a href="./" class="text-xl py-1">Youtube</a>
+                <a href="./" class="text-xl py-1">Linkedin</a>
             </div>
             <div class="flex flex-col text-[#0f0f0f] dark:text-gray-100">
                 <h6 class="text-3xl font-extrabold pb-2">Popular Brands</h6>
-                <a href="../components" class="text-xl py-1 hover:text-gray-400">Lamborghini</a>
-                <a href="../components" class="text-xl py-1">Rolls Royce</a>
-                <a href="../components" class="text-xl py-1">Ferrari</a>
-                <a href="../components" class="text-xl py-1">Porsche</a>
-                <a href="../components" class="text-xl py-1">McLaren</a>
+                <a href="./fleet/lamborghini" class="text-xl py-1 hover:text-gray-400">Lamborghini</a>
+                <a href="./fleet/rolls-royce" class="text-xl py-1 hover:text-gray-400">Rolls Royce</a>
+                <a href="./fleet/ferrari" class="text-xl py-1 hover:text-gray-400">Ferrari</a>
+                <a href="./fleet/porsche" class="text-xl py-1 hover:text-gray-400">Porsche</a>
+                <a href="./fleet/mclaren" class="text-xl py-1 hover:text-gray-400">McLaren</a>
             </div>
         </div>
     </div>
