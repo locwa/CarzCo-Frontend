31cd9cd99539b2655f80938a568932423d490a23
locwa <94048557+locwa@users.noreply.github.com>
2025-02-09 10:58:53 +0800

    added burger and dropdown list for mobile

---
+++ resources/views/components/menu-burger.blade.php
@@ -0,0 +1,5 @@
+<svg class="h-10 w-10" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 18L20 18" class="stroke-[#0f0f0f] dark:stroke-gray-100" stroke-width="2" stroke-linecap="round"/>
+    <path d="M4 12L20 12" class="stroke-[#0f0f0f] dark:stroke-gray-100" stroke-width="2" stroke-linecap="round"/>
+    <path d="M4 6L20 6" class="stroke-[#0f0f0f] dark:stroke-gray-100" stroke-width="2" stroke-linecap="round"/>
+</svg>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,8 +1,34 @@
 <nav class="flex justify-between p-4 items-center">
     <a href="{{ route('home') }}"><x-carzco-logo></x-carzco-logo></a>
-    <div class="flex gap-10 mr-6">
-        <a href="{{route('fleet')}}" class="text-[#0f0f0f] dark:text-gray-100 text-lg hover:text-gray-400">Our Fleet</a>
-        <a href="{{route('services')}}" class="text-[#0f0f0f] dark:text-gray-100 text-lg hover:text-gray-400">Services</a>
-        <a href="{{route('about')}}" class="text-[#0f0f0f] dark:text-gray-100 text-lg hover:text-gray-400">About</a>
+
+    <div class="gap-10 mr-6 md:flex hidden">
+        <x-nav-links href="{{route('fleet')}}">Our Fleet</x-nav-links>
+        <x-nav-links href="{{route('services')}}">Services</x-nav-links>
+        <x-nav-links href="{{route('about')}}">About</x-nav-links>
+    </div>
+
+    <!-- Mobile nav links -->
+    <div class="md:hidden h-20">
+        <button class="h-full" id="navButton" onclick="navMenuControls()">
+            <x-menu-burger></x-menu-burger>
+        </button>
     </div>
 </nav>
+<div class="md:hidden text-[#0f0f0f] dark:text-gray-100 bg-gray-300 dark:bg-[#1f1f1f] flex-col" id="navMenu" style="display: none">
+    <x-nav-links class="py-2 px-4" href="{{route('fleet')}}">Our Fleet</x-nav-links>
+    <x-nav-links class="py-2 px-4" href="{{route('services')}}">Services</x-nav-links>
+    <x-nav-links class="py-2 px-4" href="{{route('about')}}">About</x-nav-links>
+</div>
+
+<script>
+
+    function navMenuControls() {
+        let dropdown = document.getElementById("navMenu");
+        if (dropdown.style.display === "none"){
+            dropdown.style.display = "flex"
+        } else {
+            dropdown.style.display = "none"
+        }
+    }
+
+</script>
