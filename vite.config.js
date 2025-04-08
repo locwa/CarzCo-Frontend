f1ccc60f5775a2c0431f870563d8dc390d0499f9
locwa <94048557+locwa@users.noreply.github.com>
2025-04-09 01:20:17 +0800

    made base configuration a comment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
     // Base is configured for build in production-dev environment. Remove base if deploying on a proper server with proper configuration
-    base: '/CarzCo/CarzCo-Frontend/public/build',
+    // base: '/CarzCo/CarzCo-Frontend/public/build',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
